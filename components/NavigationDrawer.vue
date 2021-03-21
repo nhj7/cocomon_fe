9e0fb298b88bb67ff04cdd219943d03ad66e79ea
nhj12311 <nhj12311@gmail.com>
2021-03-21 15:15:41 +0900

    git initial commit

---
+++ components/NavigationDrawer.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="NavigationDrawer">
+    <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">COCOMON</v-list-item-title>
+          <v-list-item-subtitle>crypto concurrency</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-icon @click="$vuetify.theme.dark = !$vuetify.theme.dark;" >mdi-cog</v-icon>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+
+      
+    };
+  } // end data.
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,171 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <NavigationDrawer />
+    </v-navigation-drawer>
+    
+    <v-system-bar
+      fixed
+      lights-out abolute="true" app height="30px"
+    >
+      <span>한국 프리미엄 3.58%</span>
+      <v-spacer></v-spacer>
+    </v-system-bar>
+
+
+    
+    <v-main>
+      <v-row justify="center" align="center">
+        <v-col cols="2" sm="8" md="6"></v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" sm="8" md="6">
+          <v-card>
+            <v-card-title>
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+              ></v-text-field>
+            </v-card-title>
+            <v-data-table :headers="headers" :items="desserts" :search="search"></v-data-table>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-main>
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+
+  methods: {
+    setColor: color => {
+      console.log($nuxt, color);
+
+      try {
+        $nuxt.$vuetify.theme.dark = ["dark", "system"].includes(color);
+        //if( )
+        $nuxt.$vuetify.theme.sepia = true;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+  data() {
+    return {
+      search: "",
+      colors: ["system", "light", "dark", "sepia"],
+      headers: [
+        {
+          text: "Dessert (100g serving)",
+          align: "start",
+          filterable: false,
+          value: "name"
+        },
+        { text: "Calories", value: "calories" },
+        { text: "Fat (g)", value: "fat" },
+        { text: "Carbs (g)", value: "carbs" },
+        { text: "Protein (g)", value: "protein" },
+        { text: "Iron (%)", value: "iron" }
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: "1%"
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: "1%"
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: "7%"
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: "8%"
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: "16%"
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: "0%"
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: "2%"
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: "45%"
+        },
+        {
+          name: "Donut",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: "22%"
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: "6%"
+        }
+      ]
+    };
+  } // end data.
+};
+</script>
+
