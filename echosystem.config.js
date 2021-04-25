0efef64998c53e1235ad3685251000051d77efd0
nahyung <nhj12311@gmail.com>
2021-04-25 10:08:24 +0900

    add pm2 cluster

--- echosystem.config.js
+++ echosystem.config.js
@@ -1,45 +1,15 @@
-module.exports = {
-    apps: [
-      {
-        name: 'sample-app',
-        // package.json에 정의된 npm run start를 실행하게 해서 PM2로 관리하게 한다.
-        script: 'npm',
-        args: 'run start',
-        instances: 2,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '1G',
-        // 개발 환경에서 적용될 설정
-        // pm2 start ecosystem.config.js
-        env: {
-          HOST: '0.0.0.0',
-          PORT: 5000,
-          NODE_ENV: 'development'
-        },
-        // 배포 환경에서 적용될 설정
-        // pm2 start ecosystem.config.js --env production
-        env_production: {
-          HOST: '0.0.0.0',
-          PORT: 5000,
-          NODE_ENV: 'production'
-        },
-        output: './logs/console.log',
-        error: './logs/consoleError.log'
-      }
-    ],
-  
-    deploy: {
-      production: {
-        // sample
-        user: 'node',
-        host: '123.12.123.1',
-        ref: 'origin/master',
-        repo: 'git@github.com:repo.git',
-        path: '/var/www/production',
-        'post-deploy':
-          'npm install && pm2 reload ecosystem.config.js --env production'
-      }
-    }
-  }
+//ecosystem.config.js
 
-  // pm2 start ecosystem.config.js --env production
+const isProd = process.env.NODE_ENV == "production";
+
+const config = {
+  apps: [{
+    name: 'cocomon',
+    script: 'npm',
+    args : isProd ? 'run start' : 'run dev',
+    instances: 2,
+    exec_mode: `cluster`
+    }]
+}
+
+module.exports = config;
