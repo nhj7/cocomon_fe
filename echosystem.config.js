3ad3e6ea210c3a05a2890c58a927d35f40f393a9
nhj12311 <nhj12311@gmail.com>
2021-04-26 00:04:55 +0900

    cc

--- echosystem.config.js
+++ echosystem.config.js
@@ -5,17 +5,17 @@ const isProd = process.env.NODE_ENV == "production";
 const config = {
   apps: [{
     name: 'cocomon',
-    //script: './node_modules/nuxt/bin/nuxt.js',
-    script: 'nuxt',
+    script: './node_modules/nuxt/bin/nuxt.js',
+    //script: 'nuxt',
     args : 'start',
     //instances: 'max',
     instances: 2,
     exec_mode: 'cluster',
-    //exec_mode: 'fork's
-    wait_ready: true,
-    listen_timeout: 3000,
-    error_file : "/log/cocomon/err.log",
-    out_file : "/log/cocomon/out.log"
+    //exec_mode: 'fork',
+    //wait_ready: true,
+    //listen_timeout: 3000,
+    //error_file : "/log/cocomon/err.log",
+    //out_file : "/log/cocomon/out.log"
     }]
 }
 
