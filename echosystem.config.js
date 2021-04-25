b4f93a3df21c3a18c12f9e7bd35969c157226775
nhj12311 <nhj12311@gmail.com>
2021-04-25 23:42:59 +0900

    pm2 update 33

--- echosystem.config.js
+++ echosystem.config.js
@@ -5,10 +5,11 @@ const isProd = process.env.NODE_ENV == "production";
 const config = {
   apps: [{
     name: 'cocomon',
-    script: './node_modules/nuxt/bin/nuxt.js',
-    args : 'start',
+    //script: './node_modules/nuxt/bin/nuxt.js',
+    script: 'npm',
+    args : 'run start',
     //instances: 'max',
-    instances: 0,
+    instances: 2,
     exec_mode: 'cluster',
     //exec_mode: 'fork's
     wait_ready: true,
