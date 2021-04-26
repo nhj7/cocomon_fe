cf1b2ba8a56d8338d467fff008d9136672e45cd7
nahyung <nhj12311@gmail.com>
2021-04-26 21:01:34 +0900

    update process.env

--- nuxt.config.js
+++ nuxt.config.js
@@ -154,6 +154,51 @@ if( process.env.NODE_ENV == "production"){
     key: fs.readFileSync('/etc/letsencrypt/live/cocomon.kr/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/cocomon.kr/fullchain.pem')
   }
+
+
+  process.env = {
+    LESSOPEN : '| /usr/bin/lesspipe %s',
+    MAIL: '/var/mail/github',
+    USER: 'github',
+    LANGUAGE: 'en_US:en',
+    npm_config_user_agent: 'npm/7.11.1 node/v14.16.0 linux x64',
+    npm_node_execpath: '/usr/local/bin/node',
+    SHLVL: '1',
+    npm_config_noproxy: '',
+    HOME: '/home/github',
+    OLDPWD: '/app/actions-runner/_work/cocomon_fe',
+    npm_package_json: '/app/actions-runner/_work/cocomon_fe/cocomon_fe/package.json',
+    npm_config_userconfig: '/home/github/.npmrc',
+    COLOR: '1',
+    npm_config_metrics_registry: 'https://registry.npmjs.org/',
+    LOGNAME: 'github',
+    _: '/usr/local/bin/npm',
+    npm_config_prefix: '/usr/local',
+    TERM: 'xterm',
+    npm_config_cache: '/home/github/.npm',
+    npm_config_node_gyp: '/usr/local/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js',
+    PATH: '/app/actions-runner/_work/cocomon_fe/cocomon_fe/node_modules/.bin:/app/actions-runner/_work/cocomon_fe/node_modules/.bin:/app/actions-runner/_work/node_modules/.bin:/app/actions-runner/node_modules/.bin:/app/node_modules/.bin:/node_modules/.bin:/usr/local/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games',
+    NODE: '/usr/local/bin/node',
+    npm_package_name: 'cocomon_fe',
+    DISPLAY: 'localhost:10.0',
+    LANG: 'en_US.UTF-8',
+    LS_COLORS: 'rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:',
+    npm_lifecycle_script: 'nuxt start',
+    SHELL: '/bin/bash',
+    npm_package_version: '1.0.0',
+    npm_lifecycle_event: 'start',
+    LESSCLOSE: '/usr/bin/lesspipe %s %s',
+    npm_config_globalconfig: '/usr/local/etc/npmrc',
+    npm_config_init_module: '/home/github/.npm-init.js',
+    PWD: '/app/actions-runner/_work/cocomon_fe/cocomon_fe',
+    npm_execpath: '/usr/local/lib/node_modules/npm/bin/npm-cli.js',
+    npm_command: 'run-script',
+    INIT_CWD: '/app/actions-runner/_work/cocomon_fe/cocomon_fe',
+    EDITOR: 'vi',
+    NODE_ENV: 'production',
+    _applied: 'true'
+  }
+  
 }
 
 /*
