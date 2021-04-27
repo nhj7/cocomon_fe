48f93594287e78765068fd3c3bba98e91e72e382
nahyung <nhj12311@gmail.com>
2021-04-28 07:03:08 +0900

    update ci 88

--- startNuxt.js
+++ startNuxt.js
@@ -1,23 +1,26 @@
 const { loadNuxt, build } = require('nuxt')
+const config = require('../nuxt.config.js')
 
 const app = require('express')()
 const isDev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 3000
+const port = config.server.https ? 8080 : 4443;
 
 async function start() {
   // We get Nuxt instance
   const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
-
   // Render every route with Nuxt.js
   app.use(nuxt.render)
-
   // Build only in dev mode with hot-reloading
   if (isDev) {
     build(nuxt)
   }
-  // Listen the server
-  app.listen(port, '0.0.0.0')
+  if( config.server.https ){
+    const https = require('https');
+    https.createServer(config.server.https, app).listen(port);
+  }else{
+    // Listen the server
+    app.listen(port, '0.0.0.0')
+  }  
   console.log('Server listening on `localhost:' + port + '`.')
 }
-
 start()
