73ef1231c900e0c7f9e9d298b0e4e1acc1e8af30
nahyung <nhj12311@gmail.com>
2021-05-02 02:39:35 +0900

    fix echosystem env

--- echosystem.config.js
+++ echosystem.config.js
@@ -14,14 +14,14 @@ const config = {
     //wait_ready: true,
     //listen_timeout: 3000,
     env: {
-      NODE_ENV: "development",
-      httpPort: 8081,
-      httpsPort: 8443,
+      "NODE_ENV": "development",
+      "httpPort": 8081,
+      "httpsPort": 8443,
     },
     env_production: {
-      NODE_ENV: "production",
-      httpPort: 8080,
-      httpsPort: 4443,
+      "NODE_ENV": "production",
+      "httpPort": 8080,
+      "httpsPort": 4443,
     }
   }, {
     name: 'cocomon_dev',
@@ -37,14 +37,14 @@ const config = {
     //wait_ready: true,
     //listen_timeout: 3000,
     env: {
-      NODE_ENV: "production",
-      httpPort: 8081,
-      httpsPort: 8443,
+      "NODE_ENV": "production",
+      "httpPort": 8081,
+      "httpsPort": 8443,
     },
     env_production: {
-      NODE_ENV: "production",
-      httpPort: 8081,
-      httpsPort: 8443,
+      "NODE_ENV": "production",
+      "httpPort": 8081,
+      "httpsPort": 8443,
     }
   }
   ]
