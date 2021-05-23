0d813b4996e16beb488e79fadb8cbfb85f166abe
nhj.win10 <nhj12311@gmail.com>
2021-05-23 10:13:32 +0900

    update news tmp

--- server/util/redis.js
+++ server/util/redis.js
@@ -13,3 +13,21 @@ const redisClient = redis.createClient({
 redisClient.on("error", function(error) {
     console.error(error);
 });
+
+const rpush = (key, data) => {
+    redisClient.rpush(key, JSON.stringify(data));
+}
+
+const ltrim = (key, start, stop ) => {
+    redisClient.ltrim(key, start , stop);
+}
+
+const end = () => {
+    redisClient.end(true);
+}
+
+module.exports = {
+    rpush : rpush
+    , ltrim : ltrim
+    , end : end
+}
