e276c025cecfbf3eeb2d5d06b3c7d8accb1c2734
nhj12311 <nhj12311@gmail.com>
2021-09-02 22:50:44 +0900

    fix news message

--- test/test_markdown.js
+++ test/test_markdown.js
@@ -1,7 +1,8 @@
 const marked = require("marked");
 
-//const markdownString = `<h2> [ㅋㅋㅋㅋ](https://www.google.co.kr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png) </h2> `;
+const markdownString = ` ![ㅋㅋㅋㅋ](https://www.google.co.kr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png)
+### abcd `;
 
-const markdownString = `### [ㅋㅋㅋㅋ](https://www.google.co.kr)  `;
+//const markdownString = `### [ㅋㅋㅋㅋ](https://www.google.co.kr)  `;
 
 console.log(marked(markdownString));
